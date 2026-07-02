dc1dbf4bb2e6ca8226286102e279d525dad3e499
poornimaragav <102597416+poornimaragav@users.noreply.github.com>
2026-07-02 13:28:54 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,7 @@
-output "ec2_public_ip" {}
+output "ec2_public_ip" {
+  value = module.ec2.public_ip
+}
 
-output "grafana_url" {}
-
-output "nodered_url" {}
+output "bucket_name" {
+  value = module.s3.bucket_name
+}
